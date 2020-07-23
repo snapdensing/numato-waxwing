4aa594c11f48863b7c5998f6927ec8df2a22e416
snapdensing <snapdensing@gmail.com>
2020-07-23 19:49:26 +0800

    UART Rx single sample: State machine only

---
+++ uart/tb_uartrx_simple.v
@@ -0,0 +1,83 @@
+`timescale 1ns / 1ps
+`define CLK_PERIOD 10 //10ns
+`define BAUD_MULT 10416 //10416 clks = 1 bit
+module tb_uartrx_simple;
+
+	// Inputs
+	reg clk;
+	reg nrst;
+	reg en;
+	reg rx;
+
+	// Outputs
+	wire [7:0] dout;
+  wire out_state;
+  wire [2:0] out_sample_ctr;
+
+	// Instantiate the Unit Under Test (UUT)
+	uartrx_simple uut (
+		.clk(clk), 
+		.nrst(nrst), 
+		.en(en), 
+		.rx(rx), 
+		.dout(dout),
+    .out_state(out_state),
+    .out_sample_ctr(out_sample_ctr)
+	);
+  
+  always begin
+    #(`CLK_PERIOD/2) clk = ~clk;
+  end
+
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		nrst = 0;
+		en = 0;
+		rx = 1'b1;
+
+		// Wait 100 ns for global reset to finish
+		#(`CLK_PERIOD*10);
+    nrst = 1'b1;
+    #(`CLK_PERIOD*10);
+        
+		// Add stimulus here
+    #(`CLK_PERIOD * (`BAUD_MULT/2));
+    en = 1'b1;
+    #(`CLK_PERIOD * 5 * `BAUD_MULT);
+    // Start bit
+    rx = 0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 0
+    rx = 1'b1;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 1
+    rx = 0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 2
+    rx = 1'b1;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 3
+    rx = 1'b0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 4
+    rx = 1'b0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 5
+    rx = 1'b0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 6
+    rx = 1'b1;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // bit 7
+    rx = 1'b0;
+    #(`CLK_PERIOD * `BAUD_MULT);
+    // Stop bit
+    rx = 1'b1;
+    #(`CLK_PERIOD * `BAUD_MULT);
+
+    #(`CLK_PERIOD * 10);
+	end
+      
+endmodule
+
---
+++ uart/uartrx_simple.v
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+
+module uartrx_simple(
+    input clk,
+    input nrst,
+    input en,
+    input rx,
+    output [7:0] dout,
+    output out_state,
+    output [2:0] out_sample_ctr
+    );
+    
+    // Baud counter
+    parameter BAUD_PER = 10416; //9600 bits per second, 100 MHz clk (10ns period)
+    reg [13:0] baud_ctr;
+    reg baud_ctr_en;
+    always@(posedge clk)
+      if (baud_ctr < BAUD_PER) begin
+        baud_ctr = baud_ctr + 1;
+        baud_ctr_en = 0;
+      end
+      else begin
+        baud_ctr = 0;
+        baud_ctr_en = 1'b1;
+      end
+    
+    // Sampling enable
+    wire en_sample;
+    assign en_sample = en & baud_ctr_en;
+
+    // State machine
+    reg state;
+    parameter S_IDLE = 1'd0;
+    parameter S_SAMPLE = 1'd1;
+    reg [2:0] sample_ctr;
+    
+    always@(posedge clk)
+    if (!nrst)
+      state = S_IDLE;
+    else
+      if (en_sample)
+        case(state)
+          S_IDLE:
+            if (rx == 0)
+              state = S_SAMPLE;
+            else
+              state = S_IDLE;
+              
+          S_SAMPLE:
+            if (sample_ctr == 3'd7)
+              state = S_IDLE;
+            else
+              state = S_SAMPLE;
+        endcase
+
+    always@(posedge clk)
+    if (!nrst)
+      sample_ctr = 0;
+    else
+      if (en_sample)
+        case(state)
+          //S_IDLE:
+            //sample_ctr = 0;
+              
+          S_SAMPLE:
+            sample_ctr = sample_ctr + 1;
+        endcase
+
+    //dummy output
+    assign dout = 8'd0;
+    assign out_state = state;
+    assign out_sample_ctr = sample_ctr;
+endmodule
